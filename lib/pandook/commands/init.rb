1b1a5279f7e8b2bad65010819e6757aa62bd9e98
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-01-03 16:12:19 +0800

    Convert pandoc options from list to hash.

--- lib/pandook/commands/init.rb
+++ lib/pandook/commands/init.rb
@@ -21,20 +21,20 @@ lof: true                     # options: true|false
 lot: true                     # options: true|false
 content: []                   # list of files
 pandoc:
-  - from:                     # `-f FORMAT`, `-r FORMAT`, `--from=FORMAT`, `--read=FORMAT`
-  - to:                       # `-t FORMAT`, `-w FORMAT`, `--to=FORMAT`, `--write=FORMAT`
-  - output:                   # `-o FILENAME`, `--output=FILENAME`
-  - filter:                   # `-F PROGRAM`, `--filter=PROGRAM`
-  - smart: true               # `--smart`, options: true|false
-  - filter:                   # `--filter=PROGRAM`
-  - standalone:               # `--standalone`
-  - toc: true                 # `--toc-depth=NUMBER`, options: true|false
-  - include-in-header:        # `-H FILENAME`, `--include-in-header=FILENAME`
-  - include-before-body:      # `-B FILENAME`, `--include-before-body=FILENAME`
-  - include-after-body:       # `-A FILENAME`, `--include-after-body=FILENAME`
-  - html5: true               # `--html5`, options: true|false
-  - latex-engine: xelatex     # `--latex-engine=PROGRAM`
-  - latex-engine-opts:        # `--latex-engine-opt=STRING`
+  from:                     # `-f FORMAT`, `-r FORMAT`, `--from=FORMAT`, `--read=FORMAT`
+  to:                       # `-t FORMAT`, `-w FORMAT`, `--to=FORMAT`, `--write=FORMAT`
+  output:                   # `-o FILENAME`, `--output=FILENAME`
+  filter:                   # `-F PROGRAM`, `--filter=PROGRAM`
+  smart: true               # `--smart`, options: true|false
+  filter:                   # `--filter=PROGRAM`
+  standalone:               # `--standalone`
+  toc: true                 # `--toc-depth=NUMBER`, options: true|false
+  include-in-header:        # `-H FILENAME`, `--include-in-header=FILENAME`
+  include-before-body:      # `-B FILENAME`, `--include-before-body=FILENAME`
+  include-after-body:       # `-A FILENAME`, `--include-after-body=FILENAME`
+  html5: true               # `--html5`, options: true|false
+  latex-engine: xelatex     # `--latex-engine=PROGRAM`
+  latex-engine-opts:        # `--latex-engine-opt=STRING`
 EOF
 
     path = args[0]
