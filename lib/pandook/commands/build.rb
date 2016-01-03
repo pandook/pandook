334e57c22bdac9980f87038f781c991a012206ef
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-01-03 16:12:44 +0800

    Draft implementation for `pandook build` command.

--- lib/pandook/commands/build.rb
+++ lib/pandook/commands/build.rb
@@ -1,7 +1,30 @@
+require 'yaml'
+
 desc 'Build project'
 arg_name 'Build your project.'
 command :build do |c|
   c.action do |global_options,options,args|
-    puts "build command ran"
+    pandook_yaml = YAML::load(File.read('pandook.yaml'))
+
+    pandoc_input = './tmp/pandoc_input'
+    pandoc_output = 'index'
+
+    FileUtils.mkdir_p('tmp')
+
+    File.open(pandoc_input, 'w+') do |f|
+      pandook_yaml['content'].each do |content_file|
+        f.write(File.read("content/#{content_file}"))
+        f.write("\n")
+      end
+    end
+
+    from = pandook_yaml['pandoc']['from']
+    to = pandook_yaml['pandoc']['to']
+    case to
+    when 'html'
+      `pandoc #{pandoc_input} --from #{from} --to #{to} -o output/#{pandoc_output}.#{to}`
+    when 'pdf'
+      `pandoc #{pandoc_input} --from #{from} --to latex -o output/#{pandoc_output}.#{to}`
+    end
   end
 end
